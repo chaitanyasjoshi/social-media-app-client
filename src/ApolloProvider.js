605aa462923691cadcc9ac1045ec8d967297b686
Deadpool812 <chaitanya9joshi@gmail.com>
2021-06-17 11:20:26 +0530

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000',
+  uri: 'https://social-media-web.herokuapp.com/',
 });
 
 const authLink = setContext(() => {
